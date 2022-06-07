61ed43062d9a69361ae06fed9eac0df5e3967618
olic96 <francoolich@gmail.com>
2022-06-07 15:43:33 +0200

    scaf

---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header></header>
+</template>
+
+<script>
+export default {
+    name:'BaseHeader',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/blocks/BaseMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+export default {
+    name: 'BaseMain',
+}
+</script>
+
+<style>
+
+</style>
