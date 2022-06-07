b9736705b236d2743f4aef8fc9f5955d0ef2aea8
olic96 <francoolich@gmail.com>
2022-06-07 16:10:34 +0200

    scaf 2

--- src/App.vue
+++ src/App.vue
@@ -20,5 +20,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+  @import 'assets/style/global.scss';
 </style>
--- src/components/blocks/BaseHeader.vue
+++ src/components/blocks/BaseHeader.vue
@@ -1,13 +1,18 @@
 <template>
-  <header></header>
+  <header>
+      <img src="../../assets/img/spotify-logo.png" alt="logo_spotify">
+  </header>
 </template>
 
 <script>
+
 export default {
     name:'BaseHeader',
 }
 </script>
 
-<style>
-
+<style lang='scss' scoped>
+    header {
+        background-color: #2e3a46;
+    }
 </style>
