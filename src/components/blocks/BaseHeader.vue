4936d03f52e3c1c4cc391e1097d12b9cbbb8be09
olic96 <francoolich@gmail.com>
2022-06-07 16:20:14 +0200

    header

--- src/components/blocks/BaseHeader.vue
+++ src/components/blocks/BaseHeader.vue
@@ -14,5 +14,10 @@ export default {
 <style lang='scss' scoped>
     header {
         background-color: #2e3a46;
+
+        img {
+            height: 2.8125rem;
+            margin: 0.625rem 0.625rem;
+        }
     }
 </style>
